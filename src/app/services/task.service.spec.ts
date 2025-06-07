84d979f5781597ec32c635d5dafbcaaa11610ec1
Jessica Valeska <jessicavaleskas@gmail.com>
2025-06-07 14:57:57 -0300

    test(task-service): create failing test for addTask method

--- src/app/services/task.service.spec.ts
+++ src/app/services/task.service.spec.ts
@@ -13,4 +13,19 @@ describe('TaskService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should add a new task to the list', () => {
+    const task: Task = {
+      id: 1,
+      title: 'Estudar Angular',
+      completed: false,
+      createdAt: new Date()
+    };
+
+    service.addTask(task);
+
+    const tasks = service.getTasks();
+    expect(tasks.length).toBe(1);
+    expect(tasks[0]).toEqual(task);
+  });
 });
