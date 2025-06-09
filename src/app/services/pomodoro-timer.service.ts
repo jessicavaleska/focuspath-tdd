83993f6f3ed469bbb6e3c57b3d5a8938834c4381
Jessica Valeska da Silva <jessicavaleska@MacBook-Air-de-Jessica-Valeska.local>
2025-06-09 08:07:07 -0300

    feat(pomodoro-timer-service): implement PomodoroTimerService basic behaviors

--- src/app/services/pomodoro-timer.service.ts
+++ src/app/services/pomodoro-timer.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PomodoroTimerService {
+  private intervalId: any;
+  private initialTime: number = 0;
+  remainingTime: number = 0;
+  isRunning = false;
+
+  onTimerComplete = new Subject<void>();
 
   constructor() { }
+
+  start(timeInMs: number): void {
+    if (this.isRunning) {
+      return;
+    }
+
+    this.initialTime = timeInMs;
+    this.remainingTime = timeInMs;
+    this.isRunning = true;
+
+    this.intervalId = setInterval(() => {
+      if (this.remainingTime > 0) {
+        this.remainingTime -= 1000;
+      } else {
+        this.stop();
+        this.onTimerComplete.next();
+      }
+    }, 1000);
+  }
+
+  pause(): void {
+    if (this.isRunning) {
+      clearInterval(this.intervalId);
+      this.isRunning = false;
+    }
+  }
+
+  reset(): void {
+    this.pause();
+    this.remainingTime = this.initialTime;
+  }
+
+  private stop(): void {
+    clearInterval(this.intervalId);
+    this.isRunning = false;
+    this.remainingTime = 0;
+  }
 }
