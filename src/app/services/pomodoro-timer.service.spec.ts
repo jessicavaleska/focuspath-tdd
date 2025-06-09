8c5243ef75f61afa8fb6fa920008f923cf453c59
Jessica Valeska da Silva <jessicavaleska@MacBook-Air-de-Jessica-Valeska.local>
2025-06-09 08:00:40 -0300

    test(pomodoro-timer-service): create failing tests for PomodoroTimer basic behaviors

--- src/app/services/pomodoro-timer.service.spec.ts
+++ src/app/services/pomodoro-timer.service.spec.ts
@@ -8,9 +8,51 @@ describe('PomodoroTimerService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(PomodoroTimerService);
+
+    jasmine.clock().install();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    jasmine.clock().uninstall();
+  });
+
+  it('should start the timer and count down correctly', () => {
+    service.start(5000);
+
+    jasmine.clock().tick(1000);
+    expect(service.remainingTime).toBe(4000);
+
+    jasmine.clock().tick(1000);
+    expect(service.remainingTime).toBe(3000);
+  });
+
+  it('should pause the timer', () => {
+    service.start(5000);
+
+    jasmine.clock().tick(2000);
+    service.pause();
+
+    jasmine.clock().tick(2000);
+    expect(service.remainingTime).toBe(3000);
+  });
+
+  it('should reset the timer', () => {
+    service.start(5000);
+
+    jasmine.clock().tick(2000);
+    service.reset();
+
+    expect(service.remainingTime).toBe(5000);
+  });
+
+  it('should emit an event when timer reaches zero', (done) => {
+    service.onTimerComplete.subscribe(() => {
+      expect(service.remainingTime).toBe(0);
+      done();
+    });
+
+    service.start(2000);
+
+    jasmine.clock().tick(2000);
   });
 });
