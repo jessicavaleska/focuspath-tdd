dc5ce3aa4e6c68be6b2bb1e8630a3c4755de614b
Jessica Valeska <jessicavaleskas@gmail.com>
2026-04-03 17:22:09 -0300

    feat(task-list): implement delete task action

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,4 +9,10 @@ import { CommonModule } from '@angular/common';
 })
 export class TaskListComponent {
   @Input() tasks: any[] = [];
+  
+  @Output() taskDeleted = new EventEmitter<number>();
+
+  deleteTask(id: number): void {
+    this.taskDeleted.emit(id);
+  }
 }
