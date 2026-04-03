9df28c8b4342084fc5ced93fce9c06dd170b67ff
Jessica Valeska da Silva <jessicavaleska@MacBook-Air-de-Jessica-Valeska.local>
2026-04-03 14:47:14 -0300

    fix(pomodoro-timer): adjust countdown logic to emit completion event at correct time

--- src/app/services/pomodoro-timer.service.ts
+++ src/app/services/pomodoro-timer.service.ts
@@ -5,49 +5,77 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class PomodoroTimerService {
-  private intervalId: any;
-  private initialTime: number = 0;
-  remainingTime: number = 0;
+  private intervalId: ReturnType<typeof setInterval> | null = null;
+  private initialTime = 0;
+
+  remainingTime = 0;
   isRunning = false;
 
   onTimerComplete = new Subject<void>();
 
-  constructor() { }
+  constructor() {}
 
-  start(timeInMs: number): void {
+  start(timeInMs?: number): void {
     if (this.isRunning) {
       return;
     }
 
-    this.initialTime = timeInMs;
-    this.remainingTime = timeInMs;
-    this.isRunning = true;
+    if (timeInMs !== undefined) {
+      this.initialTime = timeInMs;
+      this.remainingTime = timeInMs;
+    }
 
-    this.intervalId = setInterval(() => {
-      if (this.remainingTime > 0) {
-        this.remainingTime -= 1000;
-      } else {
-        this.stop();
-        this.onTimerComplete.next();
-      }
-    }, 1000);
+    if (this.remainingTime <= 0) {
+      this.remainingTime = this.initialTime;
+    }
+
+    this.isRunning = true;
+    this.runTimer();
   }
 
   pause(): void {
-    if (this.isRunning) {
-      clearInterval(this.intervalId);
-      this.isRunning = false;
+    if (!this.isRunning) {
+      return;
     }
+
+    this.clearTimerInterval();
+    this.isRunning = false;
   }
 
   reset(): void {
-    this.pause();
+    this.clearTimerInterval();
+    this.isRunning = false;
     this.remainingTime = this.initialTime;
   }
 
-  private stop(): void {
-    clearInterval(this.intervalId);
-    this.isRunning = false;
+  private runTimer(): void {
+    this.clearTimerInterval();
+
+    this.intervalId = setInterval(() => {
+      this.decreaseTime();
+    }, 1000);
+  }
+
+  private decreaseTime(): void {
+    if (this.remainingTime > 1000) {
+      this.remainingTime -= 1000;
+      return;
+    }
+
+    this.completeTimer();
+  }
+
+  private completeTimer(): void {
     this.remainingTime = 0;
+    this.clearTimerInterval();
+    this.isRunning = false;
+    this.onTimerComplete.next();
+  }
+
+  private clearTimerInterval(): void {
+    if (this.intervalId !== null) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
   }
 }
